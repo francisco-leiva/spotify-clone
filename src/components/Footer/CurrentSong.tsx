164a35135e8f2a32fcaf68d61eb410dcc1a7bf5a
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-10 18:19:44 -0300

    Change CurrentSong component to tsx component

---
+++ src/components/Footer/CurrentSong.tsx
@@ -0,0 +1,22 @@
+import { usePlayerStore } from '@/store/playerStore'
+
+export default function CurrentSong() {
+  const currentMusic = usePlayerStore((state) => state.currentMusic)
+
+  const image = currentMusic.song?.image
+  const title = currentMusic.song?.title
+  const artist = currentMusic.song?.artists.join(', ')
+
+  return (
+    <div className='h-full flex items-center gap-3'>
+      <picture className='w-14 h-14 rounded-sm bg-[#121212]'>
+        <img className='rounded-sm' src={image} alt={title} />
+      </picture>
+
+      <div>
+        <h3>{title}</h3>
+        <span className='text-sm opacity-50'>{artist}</span>
+      </div>
+    </div>
+  )
+}
