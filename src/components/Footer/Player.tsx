e0f7fd53613f62d0d12bcdcb930eb7eba517198b
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-10 18:26:04 -0300

    Add current song src to audio tag

--- src/components/Footer/Player.tsx
+++ src/components/Footer/Player.tsx
@@ -1,50 +1,44 @@
-import { useRef, useState, useEffect } from 'react'
-import { Play } from '@/icons/Play'
-import { Pause } from '@/icons/Pause'
-import { Slider } from './Slider'
+import { useRef, useEffect } from 'react'
 import { usePlayerStore } from '@/store/playerStore'
+import { Play, Pause } from '@/icons/PlayerIcons'
+import SongTime from './SongTime'
 
 export default function Player() {
-  const { volume } = usePlayerStore((state) => ({
-    volume: state.volume,
-  }))
-  const [time, setTime] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(false)
+  const currentMusic = usePlayerStore((state) => state.currentMusic)
+  const volume = usePlayerStore((state) => state.volume)
+  const isPlaying = usePlayerStore((state) => state.isPlaying)
+  const setIsPlaying = usePlayerStore((state) => state.setIsPlaying)
   const audioRef = useRef<HTMLAudioElement | null>(null)
-  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    isPlaying ? audioRef.current?.play() : audioRef.current?.pause()
+  }, [isPlaying])
+
+  useEffect(() => {
+    const { song } = currentMusic
+    if (song && isPlaying) {
+      const src = `/music/${song.albumId}/0${song.id}.mp3`
+      if (audioRef.current) audioRef.current.src = src
+      audioRef.current?.play()
+    }
+  }, [currentMusic])
 
   useEffect(() => {
     if (audioRef.current) audioRef.current.volume = volume
   }, [volume])
 
-  useEffect(() => {
-    if (inputRef.current) {
-      const max = inputRef.current.max
-      const progress = (time / Number(max)) * 100
-      inputRef.current.style.background = `linear-gradient(to right, #fff ${progress}%, rgba(255, 255, 255, 0.3) ${progress}%)`
-    }
-  }, [time])
-
   const handleClick = () => {
-    if (isPlaying) {
-      audioRef.current?.pause()
-    } else {
-      audioRef.current?.play()
-    }
-
-    setIsPlaying((prev) => !prev)
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTime(() => e.target.valueAsNumber)
+    setIsPlaying(!isPlaying)
   }
 
   return (
     <div className='w-full h-full justify-self-center flex flex-col justify-center items-center gap-1'>
-      <audio ref={audioRef} src='/music/1/01.mp3' />
+      <audio ref={audioRef} />
 
       <div>
         <button
+          type='button'
+          aria-label='Play song'
           className='w-8 h-8 bg-white text-black rounded-full [&_svg]:mx-auto'
           onClick={handleClick}
         >
@@ -52,20 +46,7 @@ export default function Player() {
         </button>
       </div>
 
-      <div className='w-full flex items-center gap-2'>
-        <span className='text-sm opacity-50'>0:00</span>
-
-        <Slider
-          inputRef={inputRef}
-          className='flex-1'
-          value={time}
-          min={0}
-          max={240}
-          onChange={handleChange}
-        />
-
-        <span className='text-sm opacity-50'>3:40</span>
-      </div>
+      <SongTime audio={audioRef} />
     </div>
   )
 }
