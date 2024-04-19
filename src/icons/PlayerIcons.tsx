e2ea6cbc28cfc4a0379f0af7a5cfc7f27b5e2344
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-19 18:39:26 -0300

    Add big play and pause icons

--- src/icons/PlayerIcons.tsx
+++ src/icons/PlayerIcons.tsx
@@ -9,3 +9,15 @@ export const Pause = () => (
     <path d='M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
   </svg>
 )
+
+export const BigPlay = () => (
+  <svg role='img' width='24' height='24' aria-hidden='true' viewBox='0 0 24 24'>
+    <path d='m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z'></path>
+  </svg>
+)
+
+export const BigPause = () => (
+  <svg role='img' width='24' height='24' aria-hidden='true' viewBox='0 0 24 24'>
+    <path d='M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z'></path>
+  </svg>
+)
