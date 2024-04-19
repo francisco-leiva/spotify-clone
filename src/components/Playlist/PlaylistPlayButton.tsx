afe317246969914169edadba7e0a45f32ddc3fa8
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-19 18:39:52 -0300

    Add play button to playlist page

---
+++ src/components/Playlist/PlaylistPlayButton.tsx
@@ -0,0 +1,52 @@
+import { usePlayerStore } from '@/store/playerStore'
+import { BigPlay, BigPause } from '@/icons/PlayerIcons'
+import { type Playlist, type Song } from '@/lib/data'
+
+export default function PlaylistPlayButton({
+  playlistId,
+}: {
+  playlistId: string | undefined
+}) {
+  const currentMusic = usePlayerStore((state) => state.currentMusic)
+  const setcurrentMusic = usePlayerStore((state) => state.setCurrentMusic)
+  const isPlaying = usePlayerStore((state) => state.isPlaying)
+  const setIsPlaying = usePlayerStore((state) => state.setIsPlaying)
+
+  const isPlayingPlaylist =
+    isPlaying && currentMusic.playlist?.id === playlistId
+  const isPlaylistPaused =
+    !isPlaying && currentMusic.playlist?.id === playlistId
+
+  const handleClick = async () => {
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    if (isPlaylistPaused) {
+      setIsPlaying(true)
+      return
+    }
+
+    const res = await fetch(`../api/get-info-playlist.json?id=${playlistId}`)
+    const {
+      playlist,
+      song,
+      songs,
+    }: { playlist: Playlist; song: Song; songs: Song[] } = await res.json()
+
+    setIsPlaying(true)
+    setcurrentMusic({ playlist, song, songs })
+  }
+
+  return (
+    <button
+      type='button'
+      aria-label='Play playlist'
+      className='w-14 h-14 bg-green-500 text-black rounded-full [&_svg]:mx-auto hover:bg-[#1fdf64] hover:scale-105'
+      onClick={handleClick}
+    >
+      {isPlayingPlaylist ? <BigPause /> : <BigPlay />}
+    </button>
+  )
+}
