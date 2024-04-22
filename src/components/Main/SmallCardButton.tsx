e8c05bdc4bb8361955a00a87d64e24dc84fe5e70
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-22 11:11:41 -0300

    Add play button to small playlist cards

---
+++ src/components/Main/SmallCardButton.tsx
@@ -0,0 +1,56 @@
+import { usePlayerStore } from '@/store/playerStore'
+import { Play, Pause } from '@/icons/PlayerIcons'
+import { type Playlist, type Song } from '@/lib/data'
+
+export default function SmallCardButton({
+  playlistId,
+}: {
+  playlistId: string
+}) {
+  const currentMusic = usePlayerStore((state) => state.currentMusic)
+  const setcurrentMusic = usePlayerStore((state) => state.setCurrentMusic)
+  const isPlaying = usePlayerStore((state) => state.isPlaying)
+  const setIsPlaying = usePlayerStore((state) => state.setIsPlaying)
+
+  const isPlayingPlaylist =
+    isPlaying && currentMusic.playlist?.id === playlistId
+  const isPlaylistPaused =
+    !isPlaying && currentMusic.playlist?.id === playlistId
+
+  const handleClick = async (e: React.MouseEvent) => {
+    e.preventDefault()
+
+    if (isPlaylistPaused) {
+      setIsPlaying(true)
+      return
+    }
+
+    if (isPlayingPlaylist) {
+      setIsPlaying(false)
+      return
+    }
+
+    const res = await fetch(`/api/get-info-playlist.json?id=${playlistId}`)
+    const {
+      playlist,
+      song,
+      songs,
+    }: { playlist: Playlist; song: Song; songs: Song[] } = await res.json()
+
+    setIsPlaying(true)
+    setcurrentMusic({ playlist, song, songs })
+  }
+
+  return (
+    <button
+      type='button'
+      aria-label='Play playlist'
+      className={`w-8 h-8 bg-green-500 text-black rounded-full [&_svg]:mx-auto opacity-0 transition-opacity duration-200 hover:bg-[#1fdf64] shadow-[0_8px_8px_rgba(0,0,0,.3)] hover:scale-105 group-hover:opacity-100 ${
+        isPlayingPlaylist ? 'opacity-100' : ''
+      }`}
+      onClick={(e) => handleClick(e)}
+    >
+      {isPlayingPlaylist ? <Pause /> : <Play />}
+    </button>
+  )
+}
