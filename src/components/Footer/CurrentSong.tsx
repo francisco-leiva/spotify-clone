8a26a2e20ba337ee02c71816b16e788e97f2465c
francisco-leiva <leivafrancisco631@gmail.com>
2024-04-22 11:40:46 -0300

    Fix accessibility issues

--- src/components/Footer/CurrentSong.tsx
+++ src/components/Footer/CurrentSong.tsx
@@ -8,15 +8,21 @@ export default function CurrentSong() {
   const artist = currentMusic.song?.artists.join(', ')
 
   return (
-    <div className='h-full flex items-center gap-3'>
-      <picture className='w-14 h-14 rounded-sm bg-[#121212]'>
-        <img className='rounded-sm' src={image} alt={title} />
-      </picture>
+    <>
+      {currentMusic.song ? (
+        <div className='h-full flex items-center gap-3'>
+          <picture className='w-14 h-14 rounded-sm'>
+            <img className='rounded-sm' src={image} alt={title} />
+          </picture>
 
-      <div>
-        <h3>{title}</h3>
-        <span className='text-sm opacity-50'>{artist}</span>
-      </div>
-    </div>
+          <div>
+            <h3>{title}</h3>
+            <span className='text-sm opacity-50'>{artist}</span>
+          </div>
+        </div>
+      ) : (
+        <div className='w-14 h-14 rounded-sm bg-[#121212]'></div>
+      )}
+    </>
   )
 }
